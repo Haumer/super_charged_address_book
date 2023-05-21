dc6320fb33a40ffe276a12038900b41f6173fd67
Alexander <alexander.haumer@me.com>
2023-05-21 18:40:06 +0200

    added flatpicker and set config vars

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,16 +2,5 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
-class Bridge {
 
-    // Sends a message to the native app, if active.
-    static postMessage(name, data = {}) {
-      // iOS
-      window.webkit?.messageHandlers?.nativeApp?.postMessage({name, ...data})
-    }
-  }
-  
-  // Expose this on the window object so TurboNative can interact with it
-  window.Bridge = Bridge
-  export default Bridge
   
